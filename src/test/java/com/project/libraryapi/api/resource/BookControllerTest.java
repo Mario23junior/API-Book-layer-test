659bce98cacac19c0ca388087a09f1c962072428
Mario23junior <mariojunior3251@gmail.com>
2021-02-21 05:07:39 -0300

    Adicionando dependencia de validation entity

--- src/test/java/com/project/libraryapi/api/resource/BookControllerTest.java
+++ src/test/java/com/project/libraryapi/api/resource/BookControllerTest.java
@@ -2,7 +2,9 @@ package com.project.libraryapi.api.resource;
 
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -20,11 +23,9 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.libraryapi.Model.Book;
 import com.project.libraryapi.Service.BookService;
 import com.project.libraryapi.dtoModel.BookDTO;
-import com.project.libraryapi.Model.Book;
-
-import org.springframework.http.MediaType;
 
 @WebMvcTest
 @AutoConfigureMockMvc
@@ -68,7 +69,36 @@ public class BookControllerTest {
 	
 	@Test
 	@DisplayName("Lançar erro quando não tentar inserir dados invalidos")
-	public void createInvalidBookTest() {
+	public void createInvalidBookTest() throws Exception {
 		
+		String json = new ObjectMapper().writeValueAsString(new BookDTO());
+		
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+		           .post(BOOK_KEY)
+		           .contentType(MediaType.APPLICATION_JSON)
+		           .accept(MediaType.APPLICATION_JSON)
+		           .content(json);
+		
+		mvc.perform(request)
+		        .andExpect(status().isBadRequest())
+		        .andExpect(jsonPath("errors",Matchers.hasSize(3)));
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
