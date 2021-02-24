1f4e3a58382b8856cd3e54385c0cc1c850288e79
Mario23junior <mariojunior3251@gmail.com>
2021-02-24 05:14:58 -0300

    Criando primeiro teste de integração com a base de dados Book

---
+++ src/test/java/com/project/libraryapi/model/Repository/BookRepositoryTest.java
@@ -0,0 +1,57 @@
+package com.project.libraryapi.model.Repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.project.libraryapi.service.repository.BookRepository;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@DataJpaTest
+public class BookRepositoryTest {
+    
+	@Autowired
+	TestEntityManager entityManager;
+	
+	@Autowired
+	BookRepository repository;
+	
+	@Test
+	@DisplayName("Deve retornar verdadeiro quando existir um livro na base com o ISBN já informado")
+	public void returnTrueWhenIsbnExists() {
+	 
+		// cenario
+		  String isbn = "1234";
+		
+		// execucao
+		 boolean exist = repository.existsByIsbn(isbn);
+		 
+		// verificação
+		 assertThat(exist).isTrue();
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
